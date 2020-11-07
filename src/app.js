e032f17460882e0b8e41fc3b965d47877cae9b12
willame mesquita <willamecm@gmail.com>
2020-11-06 22:42:47 -0300

    metodo delete, put, post

--- src/app.js
+++ src/app.js
@@ -10,12 +10,15 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
+  //lista todos os repositorios
   return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
+  //campos para repositorios criados
   const { title, url, techs } = request.body;
 
+  //repositorio que vai ser criado
   const repository = {
     id: uuid(),
     title,
@@ -24,17 +27,53 @@ app.post("/repositories", (request, response) => {
     likes: 0,
   }
 
+  //adicionando um repositorio 
   repositories.push(repository);
 
+  //adicionando aos outros repositorios
   return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title, url, techs } = request.body;
+
+  const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(findRepositoryIndex === -1){
+    return response.status(400).json({ error: 'Repository does not exists' });
+  }
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs, 
+    likes: repositories[findRepositoryIndex].likes,
+  };
+
+  repositories[findRepositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  //pega o id
+  const { id } = request.params;
+
+  //busca o id
+  const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  console.log(findRepositoryIndex)
+
+  if(findRepositoryIndex >= 0){
+    //deleta uma posição do id
+    repositories.slice(findRepositoryIndex, 1);
+  }else{
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -42,10 +81,12 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const repository = repositories.find(repository => repository.id === id);
 
+  //verifica se o repositorio está vazio
   if(!repository){
     return response.status(400).send();
   }
 
+  //adiciona a cada like a cada requisição
   repository.likes += 1;
 
   return response.json(repository);
